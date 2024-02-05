fedc2431a019d96759004b0a4099747749b8f2af
Ijas Iqbal <ijasiqbal4@gmail.com>
2024-02-06 05:16:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Title from './component/Title';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Title />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Title.jsx
@@ -0,0 +1,37 @@
+import { ReactTyped } from "react-typed";
+import styles from './Title.module.css';
+import popping from '../assets/popping.gif';
+import { useEffect, useState } from "react";
+import bgRemoved from '../assets/bgRemoved.png';
+
+const Title = () => {
+    const [showImage, setShowImage] = useState(false);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setShowImage(true);
+            setTimeout(() => {
+                setShowImage(false);
+            }, 2000);
+        }, 4000);
+        return () => clearTimeout(timer); // Clear the timer if the component unmounts
+    }, []); 
+
+    return ( 
+        <div>
+            <div className={styles.hbdTitle}>
+            <ReactTyped
+            startWhenVisible
+              strings={[
+                "Happy Birthday Raiza",
+              ]}
+              typeSpeed={150}
+            />
+            </div>
+            {showImage && <img src={popping} alt="popping" className={styles.giff} />}
+            <img src={bgRemoved} alt="raizaPhoto" className={styles.raizaPhoto} />
+       </div>
+     );
+}
+ 
+export default Title;
